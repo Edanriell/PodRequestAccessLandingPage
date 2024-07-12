6330f78ac59ec6bbc39bc2a3eac84b1111b3a9a3
Edanriell <Edanriell@gmail.com>
2024-07-12 05:59:39 +0300

    initial  commit

---
+++ PodRequestAccessLandingPage/Program.cs
@@ -0,0 +1,24 @@
+using PodRequestAccessLandingPage.App;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
